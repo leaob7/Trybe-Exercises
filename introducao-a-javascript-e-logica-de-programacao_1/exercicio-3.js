ad6299ad379f22e6fc1560291e43869cdf3a2420
Guilherme Leao <leaob7@gmail.com>
2021-06-23 20:05:30 -0300

    Exercicio 3

---
+++ introducao-a-javascript-e-logica-de-programacao_1/exercicio-3.js
@@ -0,0 +1,11 @@
+let num1 = 100;
+let num2 = 60;
+let num3 = 30;
+
+if (num1 > num2 && num1 > num3) {
+  console.log(num1);
+} else if (num2 > num1 && num2 > num3) {
+  console.log(num2);
+} else {
+  console.log(num3);
+}
