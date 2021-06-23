3476c30260f31b883008db0c8253f0c8c91728e6
Guilherme Leao <leaob7@gmail.com>
2021-06-23 19:25:36 -0300

    Exercicio 1

---
+++ introducao-a-javascript-e-logica-de-programacao_1/aritmetica-basica.js
@@ -0,0 +1,17 @@
+let a = 10;
+let b = 4;
+
+let adicao = a + b;
+console.log(adicao);
+
+let subtracao = a - b;
+console.log(subtracao);
+
+let multiplicacao = a * b;
+console.log(multiplicacao);
+
+let divisao = a / b;
+console.log(divisao);
+
+let modulo = a % b;
+console.log(modulo);
